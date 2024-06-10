da7d6eb489647d5cb1d612e9911f726eaa90655c
Paul-prog44 <saintouenfr@hotmail.com>
2024-06-10 11:00:31 +0200

    Fetch nested pokemon data

--- src/components/DataFetchingComponent.jsx
+++ src/components/DataFetchingComponent.jsx
@@ -2,22 +2,41 @@ import { useEffect, useState } from "react"
 
 
 export default function DataFetchingComponent() {
-    const [data, setData] = useState(null)
+    const [pokemons, setPokemons] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
 
+    function fetchDataForEachPokemon(pokemon) {
+        let url = pokemon.url
+        if (fetchedUrls.has(url)) return
+
+        fetch(url)
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error('Network response was not ok')
+                }
+                return response.json()
+            })
+            .then(function(pokeData){
+                setPokemons(prevData => [...prevData, pokeData])
+        })
+    }
+
     useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon/?limit=50")
+        fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
         .then(response => {
             if(!response.ok) {
                 throw new Error('Network response was not ok')
             }
             return response.json()
         })
-        .then(data => {
-            setData(data)
+        .then(allPokemons => {
+            allPokemons.results.forEach(pokemon => {
+               fetchDataForEachPokemon(pokemon)
+            });
             setLoading(false)
         })
+        
         .catch(error => {
             setError(error)
             setLoading(false)
@@ -31,13 +50,14 @@ export default function DataFetchingComponent() {
     if(error) {
         return <div>Error : {error.message}</div>
     }
-
+    
     return (
         <div>
             <h2>Fetched Data:</h2>
-            {data.results.map(pokemon => (
+            {pokemons.map(pokemon => (
                 <li key={pokemon.name}>{pokemon.name}</li>
             ))}
         </div>
     )
+
 }
